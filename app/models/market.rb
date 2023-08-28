ecf314d1c804c7349cf00a24d37a12eefcb906be
Weston Schutt <wtschutt@gmail.com>
2023-08-28 16:28:59 -0600

    Chore: Add JSON serializer gem and update database to remove erroneous vendor_count column from Markets

--- app/models/market.rb
+++ app/models/market.rb
@@ -2,5 +2,5 @@ class Market < ApplicationRecord
   has_many :market_vendors
   has_many :vendors, through: :market_vendors
 
-  validates_presence_of :name, :street, :city, :county, :state, :zip, :lat, :lon
+  validates_presence_of :name, :street, :city, :county, :state, :zip, :lat, :lon, :vendor_count
 end
--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -1,4 +1,13 @@
 class MarketVendor < ApplicationRecord
   belongs_to :market
   belongs_to :vendor
+
+  after_save :update_vendor_count
+  after_destroy :update_vendor_count
+  
+  private
+
+  def update_vendor_count
+    market.update(vendor_count: market.vendors.count)
+  end
 end
