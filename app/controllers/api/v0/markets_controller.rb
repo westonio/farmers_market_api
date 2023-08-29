d8c5bdc73ad6dac1ea856a29e4b4e8cd45cd64c5
Weston Schutt <wtschutt@gmail.com>
2023-08-29 08:34:35 -0600

    Feature: Add route, controller action, and error serializer to implement happy/sad paths for showing a specific market

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -2,4 +2,12 @@ class Api::V0::MarketsController < ApplicationController
   def index
     render json: MarketSerializer.new(Market.all)
   end
+
+  def show
+    begin
+      render json: MarketSerializer.new(Market.find(params[:id]))
+    rescue StandardError => e
+      render json: ErrorMarketSerializer.new(e).serialized_json, status: :not_found
+    end
+  end
 end 
