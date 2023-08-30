ffb625ed1d3764b009804996fb8d215ae09397d3
Weston Schutt <wtschutt@gmail.com>
2023-08-30 10:53:39 -0600

    Feature: Add MarketVendor #create route and controller method, leveraging an exception handler for happy/sad paths

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
         resources :vendors, only: [:index]
       end
       resources :vendors, only: [:show, :create, :update, :destroy]
+      resources :market_vendors, only: [:create]
     end
   end
 end
