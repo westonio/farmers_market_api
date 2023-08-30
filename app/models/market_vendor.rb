51c845ceed324e27543a593cf9f0e3c080943572
Weston Schutt <wtschutt@gmail.com>
2023-08-30 16:58:53 -0600

    Fix: Correct message in model #cannot_already_exist custom error method

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -3,11 +3,11 @@ class MarketVendor < ApplicationRecord
   belongs_to :vendor
   
   validate :cannot_already_exist, on: :create
-
+  
 private
   def cannot_already_exist
     if MarketVendor.exists?(market_id: market_id, vendor_id: vendor_id)
-      errors.add(:base, "Market vendor asociation between market with market_id=#{market.id} and vendor_id=#{vendor.id} already exists")
+      errors.add(:base, "MarketVendor association between market with market_id=#{market.id} and vendor_id=#{vendor.id} already exists")
     end
   end
 end
