e46ac368002eb6a8e4d2fc70228b82b1e21a40bc
Weston Schutt <wtschutt@gmail.com>
2023-08-30 11:11:22 -0600

    Model: Add #cannot_already_exist custom validation method called upong #create

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -1,4 +1,13 @@
 class MarketVendor < ApplicationRecord
   belongs_to :market
   belongs_to :vendor
+  
+  validate :cannot_already_exist, on: :create
+
+private
+  def cannot_already_exist
+    if MarketVendor.exists?(market_id: market_id, vendor_id: vendor_id)
+      errors.add(:base, "Market vendor asociation between market with market_id=#{market.id} and vendor_id=#{vendor.id} already exists")
+    end
+  end
 end
