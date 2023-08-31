0c6fe4b378b720855af6b175386d8bfc5b65e0a1
Weston Schutt <wtschutt@gmail.com>
2023-08-31 12:14:46 -0600

    Model: Add Market class methods to enable searching for markets using specific allowed / disallowed parameters

--- app/models/market.rb
+++ app/models/market.rb
@@ -3,4 +3,42 @@ class Market < ApplicationRecord
   has_many :vendors, through: :market_vendors
 
   validates_presence_of :name, :street, :city, :county, :state, :zip, :lat, :lon
+
+  private
+  
+  def self.search(params)
+    if validate_params(params)
+      find_markets(params)
+    else
+      raise SearchError, "Invalid set of parameters. Please provide a valid set of parameters to perform a search with this endpoint."
+    end
+  end
+
+  def self.find_markets(params)
+    markets = Market.all
+    markets = filter_name(markets, params[:name]) if params.has_key?(:name)
+    markets = filter_city(markets, params[:city]) if params.has_key?(:city)
+    markets = filter_state(markets, params[:state]) if params.has_key?(:state)
+    markets
+  end
+  
+  def self.validate_params(params)
+    current_params = params.keys.reject { |key| key == "controller" || key == "action" }
+    current_search = current_params.map &:to_sym
+    valid_search = [[:state], [:city, :state], [:city, :name, :state], [:name, :state], [:name]]
+ 
+    valid_search.include?(current_search.sort)
+  end
+
+  def self.filter_name(markets, name)
+    markets.where("name ILIKE ?", "%#{name}%")
+  end
+  
+  def self.filter_city(markets, city)
+    markets.where("city ILIKE ?", "%#{city}%")
+  end
+  
+  def self.filter_state(markets, state)
+    markets.where("state ILIKE ?", "%#{state}%")
+  end
 end
