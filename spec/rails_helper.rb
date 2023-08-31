79360e5b323e49d16922e64fdd5b69ae591819f0
Weston Schutt <wtschutt@gmail.com>
2023-08-31 17:43:22 -0600

    Chore: Add configuration for gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,3 +72,13 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data('DONT_SHARE_MY_TMDB_SECRET_TOKEN') do
+    Rails.application.credentials.tomtom[:key]
+  end
+  config.configure_rspec_metadata!
+  config.default_cassette_options = { serialize_with: :json, record: :new_episodes }
+end
